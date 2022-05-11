ecc922ff1e351b7ff793ef81294c4aee7e2ca7c1
Nikola Đokić <nd51917@fer.hr>
2022-05-11 12:19:23 +0200

    Initial commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Nikola Đokić on 11.05.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
