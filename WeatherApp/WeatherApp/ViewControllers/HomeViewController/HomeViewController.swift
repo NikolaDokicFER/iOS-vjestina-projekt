ffded143fa32f598d38ccf570d77fd7ab7b68f61
Nikola Đokić <92534914+NikolaDokicFER@users.noreply.github.com>
2022-06-03 09:48:06 +0200

    Revert "Nikola gr"

--- WeatherApp/WeatherApp/ViewControllers/HomeViewController/HomeViewController.swift
+++ WeatherApp/WeatherApp/ViewControllers/HomeViewController/HomeViewController.swift
@@ -8,7 +8,7 @@ class HomeViewController: UIViewController {
     private var stackView: UIStackView!
     
     //Nikola
-    private var mainView: MainView!
+    private var mainView: UIView!
     
     //current day forcats
     private var dayForcatsView: UIView!
@@ -115,16 +115,10 @@ class HomeViewController: UIViewController {
     
     //Nikola
     private func buildMainView() {
-<<<<<<< HEAD
-        mainView = MainView()
-        mainView.backgroundColor = .systemBlue
-=======
         mainView = UIView()
         mainView.backgroundColor = StyleConstants.AppColors.lightBlue
->>>>>>> 72d78bc419e2b035495eafffc5b4e2ebd26e1c9f
         mainView.layer.cornerRadius = 30
         stackView.addArrangedSubview(mainView)
-        
     }
     
     private func buildDayForcatsView() {
@@ -214,13 +208,8 @@ class HomeViewController: UIViewController {
     //Nikola
     private func mainViewConstraints() {
         mainView.snp.makeConstraints({
-<<<<<<< HEAD
-            $0.top.leading.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(450)
-=======
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(500)
->>>>>>> 72d78bc419e2b035495eafffc5b4e2ebd26e1c9f
         })
     }
     
