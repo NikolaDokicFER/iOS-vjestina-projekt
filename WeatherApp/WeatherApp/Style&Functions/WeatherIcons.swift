ccb4188c0494331842bf0e5523b98abca642bc01
mikulicJurica <jurica.mikulic18@gmail.com>
2022-06-09 22:59:40 +0200

    icons

--- WeatherApp/WeatherApp/Style&Functions/WeatherIcons.swift
+++ WeatherApp/WeatherApp/Style&Functions/WeatherIcons.swift
@@ -12,21 +12,21 @@ struct WeatherIcons {
     func weatherIcon(conditionId: Int) -> String {
         switch conditionId {
         case 200...232:
-            return "cloud.bolt"
+            return "cloud.bolt.rain.fill"
         case 300...321:
-            return "cloud.drizzle"
+            return "cloud.drizzle.fill"
         case 500...531:
             return "cloud.rain"
         case 600...622:
-            return "cloud.snow"
+            return "cloud.snow.fill"
         case 701...781:
-            return "cloud.fog"
+            return "cloud.fog.fill"
         case 800:
-            return "sun.max"
+            return "sun.max.fill"
         case 801...804:
-            return "cloud.bolt"
+            return "cloud.bolt.fill"
         default:
-            return "cloud"
+            return "cloud.fill"
         }
     }
     
