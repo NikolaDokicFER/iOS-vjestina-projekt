0ff23e17c27be748318cd217c39fd98d968506ee
Nikola Đokić <nd51917@fer.hr>
2022-06-10 00:50:20 +0200

    Mainview fix

--- WeatherApp/WeatherApp/Model/CityNameModel.swift
+++ WeatherApp/WeatherApp/Model/CityNameModel.swift
@@ -8,13 +8,19 @@
 import Foundation
 
 struct CityNameModel: Codable{
-    var lon: Float
-    var lat: Float
-    var name: String
+    let lon: Float
+    let lat: Float
+    let name: String
     
     init(fromModel model: City) {
         self.lon = model.lon
         self.lat = model.lat
         self.name = model.name!
+    }
+    
+    init(name: String, lon: Float, lat: Float){
+        self.lon = lon
+        self.lat = lat
+        self.name = name
     }
 }
